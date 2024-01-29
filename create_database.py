c78672d5adfcae61b5390361b6344ad9a6bb82f6
Cory Meals <57822121+cmeals2507@users.noreply.github.com>
2024-01-29 15:48:38 -0600

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -2,12 +2,22 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores.chroma import Chroma
+from langchain.vectorstores.faiss import FAISS
 import os
 import shutil
 
-CHROMA_PATH = "chroma"
-DATA_PATH = "data/books"
+from langchain.embeddings import OpenAIEmbeddings
+
+from langchain.embeddings import OpenAIEmbeddings
+
+openai_ef = OpenAIEmbeddings(
+                api_key="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",
+                model_name="text-embedding-3-small"
+            )
+
+
+CHROMA_PATH = "/Users/cdmeals/opt/anaconda3/lib/python3.9/site-packages/chromadb"
+DATA_PATH = "/Users/cdmeals/Library/CloudStorage/Dropbox/UH 2023.2024/Research AY 2023-2024/AI Rubric/Code/data"
 
 
 def main():
@@ -17,7 +27,7 @@ def main():
 def generate_data_store():
     documents = load_documents()
     chunks = split_text(documents)
-    save_to_chroma(chunks)
+    save_to_faiss(chunks)
 
 
 def load_documents():
@@ -28,8 +38,8 @@ def load_documents():
 
 def split_text(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=100,
+        chunk_size=1000,
+        chunk_overlap=30,
         length_function=len,
         add_start_index=True,
     )
@@ -43,18 +53,17 @@ def split_text(documents: list[Document]):
     return chunks
 
 
-def save_to_chroma(chunks: list[Document]):
+def save_to_faiss(chunks: list[Document]):
     # Clear out the database first.
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
     # Create a new DB from the documents.
-    db = Chroma.from_documents(
+    db = FAISS.from_documents(
         chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
     )
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
-
 
 if __name__ == "__main__":
     main()
